d6dacbdd084808d6baf67717ab49938d8c4a0c0d
hyuntony <ankim90@gmail.com>
2021-07-21 11:59:03 -0400

    exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+end
+
+store1 = Store.find_by(id: 1)
+store2 = Store.find_by(id: 2)
+
+store1.name = 'something else'
+store1.save
